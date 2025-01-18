2a7a2a1708898a2335a6d2eac95dd2bf08612f52
JeongBeanHyun <kelly92108@gmail.com>
2025-01-18 15:42:15 +0900

    first commit

---
+++ GDG_SignEd/src/main/java/com/gdg/gdg_signed/GdgSignEdApplication.java
@@ -0,0 +1,13 @@
+package com.gdg.gdg_signed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdgSignEdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdgSignEdApplication.class, args);
+	}
+
+}
